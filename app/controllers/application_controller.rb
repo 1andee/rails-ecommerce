7f1e26d106c42b2ed925e53ceffb9d87d54dcb85
Ian Duke <ian.c.duke@gmail.com>
2017-07-06 20:47:53 -0400

    Updated order details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
-  def authorize
-    redirect_to '/login' unless current_user
-  end
-
   private
 
   def cart
