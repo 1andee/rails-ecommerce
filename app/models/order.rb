3e232a6a4bef07c7f1bb0e6ac55db9ac58060491
Ian Duke <ian.c.duke@gmail.com>
2017-07-07 20:28:39 -0400

    Cleaning up indenting

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,10 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_create :email_user
+
+  def email_user
+    UserMailer.order_email(self).deliver
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  after_create :email_user
+
+  def email_user
+    UserMailer.welcome_email(self).deliver
+  end
+
 end
