222b250d281236fdee7533207d12520db61afe9c
Ian Duke <ian.c.duke@gmail.com>
2017-08-08 01:12:47 -0400

    Update validations in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates_presence_of :product, :user, :description, :rating
+  validates_presence_of :product_id, :user_id, :description, :rating
 
 end
