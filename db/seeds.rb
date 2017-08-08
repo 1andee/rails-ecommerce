63fa1885306beece03a570683b20a48f6c5b3dc5
Ian Duke <ian.c.duke@gmail.com>
2017-08-08 00:38:40 -0400

    Seed products in production, too

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,6 @@ def open_asset(file_name)
   File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
 
-# Only run on development (local) instances not on production, etc.
-unless Rails.env.development?
-  puts "Development seeds only (for now)!"
-  exit 0
-end
-
 # Let's do this ...
 
 ## CATEGORIES
