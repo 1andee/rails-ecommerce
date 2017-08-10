49dea77e10e9106c18abc78441c51034aa242a7c
Ian Duke <ian.c.duke@gmail.com>
2017-08-09 22:24:45 -0400

    Remove typo in carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,6 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  =>  ENV["AWS_SECRET_ACCESS_KEY"]
   }
   config.fog_directory      = ENV["S3_BUCKET_NAME"]
-  config.fog_public         = falsec
+  config.fog_public         = false
   config.storage            = :fog
 end
