e03221c3e25a85d95ac2d8ec42aca99750cfc102
Ian Duke <ian.c.duke@gmail.com>
2017-08-09 22:22:30 -0400

    Update carrierwave initializer specs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 # Initialize app with cache_dir to bypass Heroku inhibit of /public/ dir
 CarrierWave.configure do |config|
+  config.fog_provider       = 'fog/aws'
   config.fog_credentials = {
     :provider               =>  'AWS',
     :aws_access_key_id      =>  ENV["AWS_ACCESS_KEY_ID"],
@@ -7,6 +8,5 @@ CarrierWave.configure do |config|
   }
   config.fog_directory      = ENV["S3_BUCKET_NAME"]
   config.fog_public         = false
-  # config.storage            = :fog
-  # config.fog_provider       = 'fog/aws'
+  config.storage            = :fog
 end
