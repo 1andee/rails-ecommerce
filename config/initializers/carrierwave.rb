4776a775f0a1558cbb9495527f0e1e8722eed968
Ian Duke <ian.c.duke@gmail.com>
2017-08-09 23:40:21 -0400

    Prepping to merge deployment branch with master

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               =>  'AWS',
-    :aws_access_key_id      =>  ENV["AWS_ACCESS_KEY_ID"],
-    :aws_secret_access_key  =>  ENV["AWS_SECRET_ACCESS_KEY"],
-    :region                 =>  ENV["AWS_REGION"]
-  }
-  config.fog_directory = ENV["S3_BUCKET_NAME"]
-  config.fog_public    = false
-  config.storage       = :fog
+
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_credentials = {
+      :provider               =>  'AWS',
+      :aws_access_key_id      =>  ENV["AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key  =>  ENV["AWS_SECRET_ACCESS_KEY"],
+      :region                 =>  ENV["AWS_REGION"]
+    }
+    config.fog_directory = ENV["S3_BUCKET_NAME"]
+    config.fog_public    = false
+    config.storage       = :fog
+  end
 end
