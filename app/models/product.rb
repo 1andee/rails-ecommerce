1c4247ca99b63d17f0de16cb284cb0f5468727f7
Ian Duke <ian.c.duke@gmail.com>
2017-08-09 21:07:43 -0400

    Re-work Carrierwave gem to show missing images after deployment & seeding

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,5 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :name, :price, :quantity, :category
 
+
 end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,4 @@
+# # Initialize app with cache_dir to bypass Heroku inhibit of /public/ dir
+# CarrierWave.configure do |config|
+#   config.cache_dir = "#{Rails.root}/tmp/uploads"
+# end
