01511be08edc823857f8f386e2e757fb7200ffd1
Ian Duke <ian.c.duke@gmail.com>
2017-08-09 21:43:14 -0400

    Add fog-aws gem, update CarrierWave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,10 @@
-# # Initialize app with cache_dir to bypass Heroku inhibit of /public/ dir
-# CarrierWave.configure do |config|
-#   config.cache_dir = "#{Rails.root}/tmp/uploads"
-# end
+# Initialize app with cache_dir to bypass Heroku inhibit of /public/ dir
+CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    }
+    config.fog_directory = ENV["S3_BUCKET_NAME"]
+end
