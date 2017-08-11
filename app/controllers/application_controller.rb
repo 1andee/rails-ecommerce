a81532cc7e914152be9b5b013900340ade7e4e89
Ian Duke <ian.c.duke@gmail.com>
2017-08-11 00:05:50 -0400

    Prevent logged-in users from accessing login screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
   helper_method :current_user
 
   private
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,16 @@
 class SessionsController < ApplicationController
 
+before_filter :check_session, only: [:new, :create]
+
   def create
-    if current_user
-      flash[:warning] = "You are already logged in."
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{user.firstname}!"
       redirect_to '/'
     else
-      user = User.find_by_email(params[:email])
-        if user && user.authenticate(params[:password])
-          session[:user_id] = user.id
-          flash[:success] = "Welcome back, #{user.firstname}!"
-          redirect_to '/'
-      else
-        flash[:danger] = "We couldn't log you in. Please verify that your email/password is correct."
-        redirect_to :back
-      end
+      flash[:danger] = "We couldn't log you in. Please verify that your email/password is correct."
+      redirect_to :back
     end
   end
 
